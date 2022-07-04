83691a505e227b5c3e04c98464f10e67ed9287bb
Sebastian Perez <inge.jsperez@gmail.com>
2022-07-04 18:15:50 -0500

    first commit

---
+++ src/todo/components/Title.tsx
@@ -0,0 +1,12 @@
+import { useTodos } from '../hooks/useTodos';
+
+export const Title = () => {
+
+    const { pendingTodos } = useTodos();
+
+    return (
+        <>
+            Todos: {pendingTodos}
+        </>
+    )
+}
---
+++ src/todo/components/TodoItem.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react'
+import { TodoContext } from '../context/TodoContext'
+import { useTodos } from '../hooks/useTodos'
+import { Todo } from '../interfaces/interfaces'
+
+
+interface props {
+    todo: Todo
+}
+
+export const TodoItem = ({ todo }: props) => {
+
+    // const { toggleTodo } = useContext(TodoContext)
+
+    const { toggleTodo } = useTodos();
+
+
+    const handleDbClick = () => {
+        toggleTodo(todo.id)
+    }
+
+    return (
+        <li style={{ cursor: 'pointer', textDecoration: todo.completed ? 'line-through' : '' }} onDoubleClick={handleDbClick}>
+            {todo.desc}
+        </li>
+    )
+}
